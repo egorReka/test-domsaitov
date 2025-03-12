4efb5d877661d85c3bb89c4613b1fda167614de3
egorReka <e.reka55@yandex.ru>
2025-03-12 16:17:22 +0300

    feat: добавляет header

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,22 @@
-/* в этот файл добавляет скрипты*/
+import { initToggleMenu, destroyToggleMenu } from './toggle-menu';
+
+// ---------------------------------
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  // Utils
+  // ---------------------------------
+
+
+  // Modules
+  // ---------------------------------
+
+  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+  // в load следует добавить скрипты, не участвующие в работе первого экрана
+  window.addEventListener('load', () => {
+    initToggleMenu();
+  });
+});
+
+// ---------------------------------
+
